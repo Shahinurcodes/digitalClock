49c298ff6556ee9e8e477c59c20e91fab06a4833
ShahinurCodes <shahinursul79@gmail.com>
2025-07-26 22:07:04 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function ClockTime(){
+    const today= new Date();
+    let h=today.getHours();
+    let m=today.getMinutes();
+    let s=today.getSeconds();
+    m=checkTime(m);
+    s=checkTime(s);
+    document.getElementById('txt').innerHTML= h +":"+ m +":"+ s;
+    setTimeout(ClockTime,1000);
+}
+
+
+
+function checkTime(i){
+    if(i<10){
+        i="0"+i
+    };
+    return i;
+}
+
+ClockTime();
+
+
